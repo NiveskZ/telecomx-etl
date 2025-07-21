3ea9041a8c37fa63a4704d392986d6b3a6027901
Kevin Richardson <Krsmjogos@outlook.com>
2025-07-21 20:53:28 -0300

    Análise exploratória

--- telecomx.py
+++ telecomx.py
@@ -82,36 +82,126 @@ df.columns
 # Carga e Análise
 df.describe()
 # %%
+# Avaliando Porcentagem de Churn 
+cores = sns.color_palette('Set2')
 fig, ax = plt.subplots(figsize=(10,6))
 
 ax.pie(df['Churn'].value_counts(),
-             labels=['Não Deu Churn', 'Deu Churn'],
-             autopct='%1.1f%%', explode=(0,0.1))
+             labels=['Não', 'Sim'],
+             autopct='%1.1f%%', explode=(0,0.1),colors=cores)
 ax.set_title("Clientes que deram Churn X Clientes que não deram Churn")
-# %%
-df.head()
+
+plt.tight_layout()
+plt.show()
 # %%
 fig, ax = plt.subplots(figsize=(15,5))
+fig.suptitle("Quantidade de usuários que deram Churn e não deram Churn por meses de contrato")
 
-ax = sns.countplot(df, x='tenure', hue='Churn')
-ax.set_xticks(range(1,df['tenure'].max(), 10))
+sns.countplot(df, x='tenure', hue='Churn',ax=ax)
+ax.set_xticks(range(0,df['tenure'].max(), 10))
+ax.set_xlabel("Meses de Contrato")
+ax.set_ylabel("Número de Usuários")
 
+plt.tight_layout()
+plt.show()
 # %%
+# Avaliando influência do preço de cobrança.
 fig, axs = plt.subplots(2,2,figsize=(10,6))
+fig.subplots_adjust(wspace=0.3,hspace=0.5)
 
-sns.histplot(df,x='Charges_Total',hue='Churn',ax=axs[0,0])
-sns.histplot(df,x='Charges_Day',hue='Churn',ax=axs[0,1])
-sns.histplot(df,x='Charges_Monthly',hue='Churn',ax=axs[1,0])
-sns.histplot(df,x='Charges_Total_Day',hue='Churn',ax=axs[1,1])
-# %%
-fig, axs = plt.subplots(2,2,figsize=(10,6))
+sns.histplot(df,x='Charges_Total',hue='Churn',ax=axs[0,0],palette={0:'green',1:'blue'})
+axs[0,0].set_title('Churn por total gasto')
+axs[0,0].set_xlabel('Total Gasto Pelo Cliente')
 
-sns.boxplot(df,y='Charges_Total',x='Churn',ax=axs[0,0])
-sns.boxplot(df,y='Charges_Day',x='Churn',ax=axs[0,1])
-sns.boxplot(df,y='Charges_Monthly',x='Churn',ax=axs[1,0])
-sns.boxplot(df,y='Charges_Total_Day',x='Churn',ax=axs[1,1])
+sns.histplot(df,x='Charges_Day',hue='Churn',ax=axs[0,1],palette={0:'green',1:'blue'})
+axs[0,1].set_title('Churn por total gasto diário')
+axs[0,1].set_xlabel('Total Gasto Pelo Cliente por Dia')
+
+sns.histplot(df,x='Charges_Monthly',hue='Churn',ax=axs[1,0],palette={0:'green',1:'blue'})
+axs[1,0].set_title('Churn por gasto mensal')
+axs[1,0].set_xlabel('Gasto Mensal do Cliente')
+
+sns.histplot(df,x='Charges_Total_Day',hue='Churn',ax=axs[1,1],palette={0:'green',1:'blue'})
+axs[1,1].set_title('Churn por gasto diário mensal')
+axs[1,1].set_xlabel('Gasto Mensal do Cliente por Dia')
+
+for ax in axs.ravel():
+    ax.set_ylabel("Qtd. de Clientes")
+
+plt.tight_layout()
+plt.show()
 # %%
-sns.lmplot(df, x='Charges_Total', y ='Charges_Total_Day',hue='Churn',col='Contract')
+
 # %%
-sns.lmplot(df, x='tenure', y ='Charges_Total_Day',hue='Churn',col='PaymentMethod')
+
+g = sns.lmplot(df, x='Charges_Total', y ='Charges_Total_Day',hue='Churn',col='Contract',
+               col_order=["Month-to-month","One year","Two year"], palette='Set2')
+
+g.figure.suptitle("Relação entre Total Gasto e Total Gasto por Dia",fontsize=16)
+g.figure.subplots_adjust(top=0.85)
+
+plt.tight_layout()
+plt.show()
+# %%
+fig, ax = plt.subplots(figsize=(10,6))
+fig.suptitle("Quantidade de Churn X Forma de Pagamento")
+
+sns.histplot(df,x='PaymentMethod', hue='Churn',palette={0:'green',1:'blue'},ax=ax)
+ax.set_xlabel("Forma de Pagamento")
+ax.set_ylabel("Número de Usuários")
+
+plt.tight_layout()
+plt.show()
+# %%
+g = sns.lmplot(df, x='tenure', y ='Charges_Total_Day',hue='Churn',
+               col='PaymentMethod',palette='Set2')
+g.set_axis_labels("Meses de Contrato","Total Gasto por Dia")
+
+plt.tight_layout()
+plt.show()
+# %%
+fig, axs = plt.subplots(2,1,figsize=(6,8))
+fig.subplots_adjust(hspace=0.3)
+
+sns.boxplot(df,y='Charges_Total_Day',x='Churn', hue='Churn',
+            palette='Set2',ax=axs[0])
+axs[0].set_title("Distribuição do gasto diário por status de Churn")
+
+sns.boxplot(df,y='Charges_Total_Day',
+            x='Churn', hue='Contract',
+            palette='Set2',
+            ax=axs[1])
+axs[1].set_title("Gasto diário por Churn e tipo de contrato")
+axs[1].legend(title='Contrato', bbox_to_anchor=(1.05, 1), loc='upper left')
+
+for ax in axs.ravel():
+
+    ax.set_ylabel('Total Gasto Diário')
+
+plt.tight_layout()
+plt.show()
+# %%
+# Calculando
+churn_tenure = df.groupby(['tenure','Churn']).size().unstack(fill_value=0)
+
+churn_tenure['taxa_churn'] = (churn_tenure.get(1,0)/churn_tenure.sum(axis=1)) * 100
+
+churn_tenure[['taxa_churn']]
+
+churn_tenure.reset_index(inplace=True)
+# %%
+
+fig,ax = plt.subplots(figsize=(10,5))
+
+sns.lineplot(data=churn_tenure,
+             x='tenure',
+             y='taxa_churn',
+             ax=ax)
+
+ax.set_title("Taxa de Churn por Total de Meses de Contrato do Cliente",loc='left',fontsize=16)
+ax.set_xlabel("Meses de Contrato")
+ax.set_ylabel("Taxa de Churn de cliente")
+
+plt.tight_layout()
+plt.show()
 # %%

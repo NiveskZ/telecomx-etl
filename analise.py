a93bda4ca6cd85ada4cc2828559bfed3b6e81a5e
Kevin Richardson <Krsmjogos@outlook.com>
2025-07-22 12:29:23 -0300

    salvando arquivos

--- analise.py
+++ analise.py
@@ -1,84 +1,10 @@
 # %%
-import requests
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-#%%
-# Extração
-# Solicitando os dados
-url = 'https://raw.githubusercontent.com/ingridcristh/challenge2-data-science/refs/heads/main/TelecomX_Data.json'
-response = requests.get(url)
-
-# Transformando em objeto python
-dados = response.json()
-
-# Convertendo os dados em um DataFrame do Pandas
-df = pd.json_normalize(dados)
-df.head()
 # %%
-# Transformação
-# Verificando os tipos de dados
-df.info()
-# %%
-# Verificando se possui valores nulos
-df.isnull().sum()
-# %%
-# Verificando se possui valores vazios em alguma coluna.
-for col in df.columns:
-    print(f"Valores únicos em {col}: {df[col].nunique()}")
-    if df[col].nunique() < 10:
-        print(df[col].unique())
-        print("\n")
-# %%
-# Contando os valorez vazios.
-vazios = df.apply(lambda x: x.astype(str).str.strip() == '').sum()
-vazios
-# %%
-# Calculando Potencial de informação perdida ao remover linhas com churn vazio
-print(f"Porcentagem churn vazio: {round(vazios['Churn']/len(df),2)*100}%")
-# %%
-# Removendo as linhas com Churn vazio
-df = df[df['Churn'].str.strip() != '']
-len(df)
-# %%
-# Transformando a coluna "account.Charges.Total" em float e verificando se ainda possui nulos/vazios
-df['account.Charges.Total'] = pd.to_numeric(df['account.Charges.Total'],errors='coerce')
-total_null = df['account.Charges.Total'].isnull()
-total_null.sum()
-# %%
-# Observando se existe relação onde tenure = 0 com cobrança total nula
-(df[total_null]['customer.tenure'] == 0).all()
-# %%
-# Podemos supor que por se tratar de clientes novos ainda não possuem cobranças
-df['account.Charges.Total'] = df['account.Charges.Total'].fillna(0.)
-#%%
-# Verificando se possui linhas duplicadas
-df.duplicated().sum()
-
-# %%
-# Criando coluna de contas diárias considerando as cobranças mensais do cliente
-df["account.Charges.Day"] = ((df['account.Charges.Monthly'])/30).round(2)
-
-# Criando coluna de contas diárias considerando todas as cobranças do cliente
-df["account.Charges.Total.Day"] = ((df['account.Charges.Total']/df['customer.tenure'])/30).round(2)
-
-df.head()
-# %%
-# Padronizando colunas binárias para 0 caso Não e 1 caso Sim
-
-# Pega estritamente as colunas que tenham apenas valores "Yes" e "No"
-cols_yes_no = [col for col in df.columns if df[col].isin(["Yes","No"]).all()] 
-
-df[cols_yes_no] = df[cols_yes_no].replace({"Yes":1,"No":0})
-
-df.head()
-# %%
-# Removendo prefixos dos nomes
-df.rename(columns={
-    col:col.split('.',1)[1].replace('.','_') for col in df.columns if '.' in col
-}, inplace=True)
-df.columns
+df = pd.read_csv('data/telecomx.csv')
 # %%
 # Carga e Análise
 df.describe()
@@ -131,8 +57,6 @@ for ax in axs.ravel():
 
 plt.tight_layout()
 plt.show()
-# %%
-
 # %%
 
 g = sns.lmplot(df, x='Charges_Total', y ='Charges_Total_Day',hue='Churn',col='Contract',
@@ -206,46 +130,27 @@ for ax in axs.ravel():
 plt.tight_layout()
 plt.show()
 # %%
-# Lista das colunas de serviço, incluindo a InternetService
-servicos_cols = ['PhoneService','MultipleLines','InternetService',
-                 'OnlineSecurity','OnlineBackup','DeviceProtection',
-                 'TechSupport','StreamingTV','StreamingMovies']
-
-# Criar cópia para não alterar o original
-df_servicos = df[servicos_cols].copy()
-
-# Para colunas binárias simples, mapeia Yes->1, No->0
-binario_map = {'Yes':1, 'No':0}
-
-for col in servicos_cols:
-    if col == 'InternetService':
-        # Considera que o cliente tem internet se o valor não for 'No'
-        df_servicos[col] = df_servicos[col].apply(lambda x: 0 if x == 'No' else 1)
-    else:
-        df_servicos[col] = df_servicos[col].map(binario_map)
-
-# Agora soma linha a linha para contar quantos serviços o cliente tem
-df['n_servicos'] = df_servicos.sum(axis=1)
-df.head()
-# %%
 corr = ['n_servicos',"Charges_Total_Day","tenure","Churn"]
+# Criando matriz de correlação
 matriz_corr = df[corr].corr()
 
+# Cria uma Máscara para tirar a parte superior da matriz.
 mask = np.triu(np.ones_like(matriz_corr,dtype=bool))
 
 fig, ax = plt.subplots(figsize=(11, 9))
-
 cmap = sns.diverging_palette(180,15,as_cmap=True)
 
 sns.heatmap(matriz_corr, mask=mask,cmap=cmap, vmax=0.3, center=0,
             square=True, linewidths=.5,cbar_kws={"shrink": .5},
             annot=True, fmt=".2f",ax=ax)
 
-ax.set_title("Matriz de Correlação",fontsize=18,loc='left')
+ax.set_title("Matriz de Correlação",fontsize=24,loc='left')
 ax.set_xticks([0.5,1.5,2.5,3.5],
                labels=['Número de Serviços', 'Gasto total Diário', 'Tempo de Contrato','Churn'],
                 rotation='horizontal')
 ax.set_yticks([0.5,1.5,2.5,3.5],
                labels=['Número de Serviços', 'Gasto total Diário', 'Tempo de Contrato','Churn'],
                 rotation='horizontal')
+
+plt.show()
 # %%
---
+++ tratamento.py
@@ -0,0 +1,104 @@
+# %%
+import requests
+import pandas as pd
+#%%
+# Extração
+# Solicitando os dados
+url = 'https://raw.githubusercontent.com/ingridcristh/challenge2-data-science/refs/heads/main/TelecomX_Data.json'
+response = requests.get(url)
+
+# Transformando em objeto python
+dados = response.json()
+
+# Convertendo os dados em um DataFrame do Pandas
+df = pd.json_normalize(dados)
+df.head()
+# %%
+# Transformação
+# Verificando os tipos de dados
+df.info()
+# %%
+# Verificando se possui valores nulos
+df.isnull().sum()
+# %%
+# Verificando se possui valores vazios em alguma coluna.
+for col in df.columns:
+    print(f"Valores únicos em {col}: {df[col].nunique()}")
+    if df[col].nunique() < 10:
+        print(df[col].unique())
+        print("\n")
+# %%
+# Contando os valorez vazios.
+vazios = df.apply(lambda x: x.astype(str).str.strip() == '').sum()
+vazios
+# %%
+# Calculando Potencial de informação perdida ao remover linhas com churn vazio
+print(f"Porcentagem churn vazio: {round(vazios['Churn']/len(df),2)*100}%")
+# %%
+# Removendo as linhas com Churn vazio
+df = df[df['Churn'].str.strip() != '']
+len(df)
+# %%
+# Transformando a coluna "account.Charges.Total" em float e verificando se ainda possui nulos/vazios
+df['account.Charges.Total'] = pd.to_numeric(df['account.Charges.Total'],errors='coerce')
+total_null = df['account.Charges.Total'].isnull()
+total_null.sum()
+# %%
+# Observando se existe relação onde tenure = 0 com cobrança total nula
+(df[total_null]['customer.tenure'] == 0).all()
+# %%
+# Podemos supor que por se tratar de clientes novos ainda não possuem cobranças
+df['account.Charges.Total'] = df['account.Charges.Total'].fillna(0.)
+#%%
+# Verificando se possui linhas duplicadas
+df.duplicated().sum()
+
+# %%
+# Criando coluna de contas diárias considerando as cobranças mensais do cliente
+df["account.Charges.Day"] = ((df['account.Charges.Monthly'])/30).round(2)
+
+# Criando coluna de contas diárias considerando todas as cobranças do cliente
+df["account.Charges.Total.Day"] = ((df['account.Charges.Total']/df['customer.tenure'])/30).round(2)
+
+df.head()
+# %%
+# Padronizando colunas binárias para 0 caso Não e 1 caso Sim
+
+# Pega estritamente as colunas que tenham apenas valores "Yes" e "No"
+cols_yes_no = [col for col in df.columns if df[col].isin(["Yes","No"]).all()] 
+
+df[cols_yes_no] = df[cols_yes_no].replace({"Yes":1,"No":0})
+
+df.head()
+# %%
+# Removendo prefixos dos nomes
+df.rename(columns={
+    col:col.split('.',1)[1].replace('.','_') for col in df.columns if '.' in col
+}, inplace=True)
+df.columns
+# %%
+# Lista das colunas de serviço, incluindo a InternetService
+servicos_cols = ['PhoneService','MultipleLines','InternetService',
+                 'OnlineSecurity','OnlineBackup','DeviceProtection',
+                 'TechSupport','StreamingTV','StreamingMovies']
+
+# Criar cópia para não alterar o original
+df_servicos = df[servicos_cols].copy()
+
+# Para colunas binárias simples, mapeia Yes->1, No->0
+binario_map = {'Yes':1, 'No':0}
+
+for col in servicos_cols:
+    if col == 'InternetService':
+        # Considera que o cliente tem internet se o valor não for 'No'
+        df_servicos[col] = df_servicos[col].apply(lambda x: 0 if x == 'No' else 1)
+    else:
+        df_servicos[col] = df_servicos[col].map(binario_map)
+
+# Agora soma linha a linha para contar quantos serviços o cliente tem
+df['n_servicos'] = df_servicos.sum(axis=1)
+df.head()
+
+# %%
+df.to_csv("data/telecomx.csv")
+# %%
